c209dc24c23d1667201f6ef5f331e472466639e0
chandraabhinav68-pixe <chandraabhinav68@gmail.com>
2026-09-26 04:36:02 +0530

    Update API_URL to deployed Render backend

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://mental-health-score-5-0an5.onrender.com";
+const API_URL = "https://mental-health-score-5-0an5.onrender.com/predict";
     const form = document.getElementById("student-form");
     const button = document.getElementById("submit-button");
     const errorBox = document.getElementById("error-message");
